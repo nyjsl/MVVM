5a958f903ef68d204bf9f682eaaa7ff0e0047e1f
weixing06 <weixings06@126.com>
2020-08-30 14:42:04 +0800

    lifecycle

--- jetpack/src/main/java/org/nyjsl/jetpack/mvvm/base/BaseActivity.java
+++ jetpack/src/main/java/org/nyjsl/jetpack/mvvm/base/BaseActivity.java
@@ -1,8 +1,24 @@
 package org.nyjsl.jetpack.mvvm.base;
 
+import org.nyjsl.jetpack.mvvm.base.lifecycle.BaseViewModel;
+
+import androidx.databinding.ViewDataBinding;
+
 /**
  * @author : weixing
  * @date : 2020/8/29 2:35 PM
  */
-public abstract class BaseActivity {
+public abstract class BaseActivity<VM extends BaseViewModel,DataBinding extends ViewDataBinding> 
+extends BaseNoVMActivity<DataBinding>{
+    
+    protected VM viewModel;
+
+    @Override
+    protected DataBinding initDataBinding(int layoutId) {
+        DataBinding dataBinding = super.initDataBinding(layoutId);
+        viewModel = initViewModel();
+        return dataBinding;
+    }
+
+    protected abstract VM initViewModel();
 }
---
+++ jetpack/src/main/java/org/nyjsl/jetpack/mvvm/base/lifecycle/BaseViewModel.java
@@ -0,0 +1,12 @@
+package org.nyjsl.jetpack.mvvm.base.lifecycle;
+
+import androidx.lifecycle.ViewModel;
+
+/**
+ * @author : weixing
+ * @date : 2020/8/30 1:51 PM
+ */
+public abstract class BaseViewModel extends ViewModel {
+
+
+}
