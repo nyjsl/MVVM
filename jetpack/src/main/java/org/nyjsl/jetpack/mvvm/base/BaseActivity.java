72d73568dc26a200d3cbccc0504be823e07305db
weixing06 <weixings06@126.com>
2020-08-30 16:49:54 +0800

    base

--- jetpack/src/main/java/org/nyjsl/jetpack/mvvm/base/BaseActivity.java
+++ jetpack/src/main/java/org/nyjsl/jetpack/mvvm/base/BaseActivity.java
@@ -1,14 +1,20 @@
 package org.nyjsl.jetpack.mvvm.base;
 
+import com.blankj.utilcode.util.Utils;
+
 import org.nyjsl.jetpack.mvvm.base.lifecycle.BaseViewModel;
 
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+
 import androidx.databinding.ViewDataBinding;
+import androidx.lifecycle.ViewModelProvider;
 
 /**
  * @author : weixing
  * @date : 2020/8/29 2:35 PM
  */
-public abstract class BaseActivity<VM extends BaseViewModel,DataBinding extends ViewDataBinding> 
+public abstract class BaseActivity<VM extends BaseViewModel,DataBinding extends ViewDataBinding>
 extends BaseNoVMActivity<DataBinding>{
     
     protected VM viewModel;
@@ -16,9 +22,21 @@ extends BaseNoVMActivity<DataBinding>{
     @Override
     protected DataBinding initDataBinding(int layoutId) {
         DataBinding dataBinding = super.initDataBinding(layoutId);
-        viewModel = initViewModel();
+        viewModel = initViewModel(getViewModelClass());
         return dataBinding;
     }
 
-    protected abstract VM initViewModel();
+    private VM initViewModel(Class<VM> klazz){
+        return ViewModelProvider.AndroidViewModelFactory.getInstance(Utils.getApp()).create(klazz);
+    }
+
+    private Class<VM> getViewModelClass(){
+        //TODO 需要验证
+        ParameterizedType parametclass = (ParameterizedType) this.getClass().getGenericSuperclass();
+        Type[] actualTypeArguments = parametclass.getActualTypeArguments();
+        Class<VM> result = (Class<VM>) actualTypeArguments[0];
+        return result;
+    }
+
+
 }
--- jetpack/src/main/java/org/nyjsl/jetpack/mvvm/base/lifecycle/BaseViewModel.java
+++ jetpack/src/main/java/org/nyjsl/jetpack/mvvm/base/lifecycle/BaseViewModel.java
@@ -1,12 +1,50 @@
 package org.nyjsl.jetpack.mvvm.base.lifecycle;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
 
 /**
  * @author : weixing
  * @date : 2020/8/30 1:51 PM
  */
-public abstract class BaseViewModel extends ViewModel {
+public abstract class BaseViewModel<D> extends ViewModel {
+
+    /**
+     * 管理RxJava请求
+     */
+    private CompositeDisposable compositeDisposable;
+
+    /**
+     * 添加 rxJava 发出的请求
+     */
+    protected void addDisposable(Disposable disposable) {
+        if (compositeDisposable == null || compositeDisposable.isDisposed()) {
+            compositeDisposable = new CompositeDisposable();
+        }
+        compositeDisposable.add(disposable);
+    }
+
+    /**
+     * ViewModel销毁同时也取消请求
+     */
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        if (compositeDisposable != null) {
+            compositeDisposable.dispose();
+            compositeDisposable = null;
+        }
+    }
+
+
+    private MutableLiveData<D> data = new MutableLiveData<>();
+
+
+    public MutableLiveData<D> getData() {
+        return data;
+    }
 
 
 }
