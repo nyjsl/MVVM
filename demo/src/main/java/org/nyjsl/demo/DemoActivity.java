19a5ca3b6b65ba62b5366c7f8aa818f9cbea6f92
weixing06 <weixings06@126.com>
2020-09-18 17:17:03 +0800

    1111

--- demo/src/main/java/org/nyjsl/demo/DemoActivity.java
+++ demo/src/main/java/org/nyjsl/demo/DemoActivity.java
@@ -1,23 +1,47 @@
 package org.nyjsl.demo;
 
 
+
+import org.nyjsl.demo.bean.DemoBean;
+import org.nyjsl.demo.databinding.DemoActivityDemoBinding;
 import org.nyjsl.jetpack.mvvm.base.BaseNoVMActivity;
 
+/**
+ * databinding 绑定表达式中可以使用以上关键字和运算符
+ *
+ *  算术 + - / * %
+ *  字符串拼接 +
+ *  逻辑
+ *  位
+ *  一元
+ *  移位
+ *  关系
+ *  instanceof
+ *  组()
+ *  字面量 ,字符串 ,数字 null
+ *  方法调用
+ *  字段访问
+ *  数组访问[]
+ *  三元操作符 ?:
+ *
+ *
+ *  空合并操作符
+ */
+public class DemoActivity extends BaseNoVMActivity<DemoActivityDemoBinding> {
 
-public class DemoActivity extends BaseNoVMActivity{
+    private DemoBean db = new DemoBean();
 
     @Override
     protected void setData() {
-
     }
 
     @Override
     protected void initData() {
-
     }
 
     @Override
     protected void setModel() {
+        binding.setModel(this);
     }
 
     @Override
@@ -25,8 +49,21 @@ public class DemoActivity extends BaseNoVMActivity{
 
     }
 
+    public void increase() {
+        db.setIndex(db.getIndex()+1);
+        db.setAge(db.getAge()+1);
+        //单向绑定
+        binding.setData(db);
+    }
+
+    public void decrease() {
+        db.setIndex(db.getIndex()-1);
+        db.setAge(db.getAge()-1);
+        binding.setData(db);
+    }
+
     @Override
     protected int getLayoutId() {
-        return R.layout.demo_activity_test;
+        return R.layout.demo_activity_demo;
     }
 }
--- demo/src/main/java/org/nyjsl/demo/bean/DemoBean.java
+++ demo/src/main/java/org/nyjsl/demo/bean/DemoBean.java
@@ -1,5 +1,8 @@
 package org.nyjsl.demo.bean;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 /**
  * @author : weixing
  * @date : 2020/9/18 4:04 PM
@@ -8,6 +11,26 @@ public class DemoBean {
 
     private int index;
     private int age;
+    private boolean large;
+
+    public boolean isLarge() {
+        return large;
+    }
+
+    public void setLarge(boolean large) {
+        this.large = large;
+    }
+
+    @Nullable
+    public String getName() {
+        return name;
+    }
+
+    public void setName(@NonNull String name) {
+        this.name = name;
+    }
+
+    private String name;
 
     public int getIndex() {
         return index;
